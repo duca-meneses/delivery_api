37b10784f3b1ffb7610bbf03a9cbb3685e1f7838
Carlos Eduardo <ducameneses16@gmail.com>
2024-01-23 03:59:12 -0300

    Creating the database connection

---
+++ pizza_delivery_api/models/connection/database.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+from .secrets import connection_uri
+
+
+engine = create_engine(connection_uri, echo=True)
+
+Base = declarative_base()
+Session = sessionmaker()
---
+++ pizza_delivery_api/models/connection/secrets.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+user = os.environ['POSTGRES_USER']
+password = os.environ['POSTGRES_PASSWORD']
+database = os.environ['POSTGRES_DB']
+
+connection_uri = f'postgresql://{user}:{password}@localhost:5433/{database}'
