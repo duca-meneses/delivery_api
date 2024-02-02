58ba36d2f04871c8d5ae14f3c720506f6e7f2b0e
Carlos Eduardo <ducameneses16@gmail.com>
2024-02-02 01:59:33 -0300

    Adding jwt authentication and authorization

--- pizza_delivery_api/security.py
+++ pizza_delivery_api/security.py
@@ -2,34 +2,40 @@ from datetime import datetime, timedelta
 
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from passlib.context import CryptContext
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
-from jose import JWTError, jwt
-from passlib.context import CryptContext
 from pizza_delivery_api.models.connection.database import get_session
 from pizza_delivery_api.models.connection.secrets import Settings
 from pizza_delivery_api.models.models import User
-
-
 from pizza_delivery_api.models.schemas import TokenData
 
 settings = Settings()
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(
+        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+    )
     to_encode.update({'exp': expire})
-    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    encoded_jwt = jwt.encode(
+        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
+    )
     return encoded_jwt
+
 
 def get_password_hash(password: str):
     return pwd_context.hash(password)
 
+
 def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
+
 
 def get_current_user(
     session: Session = Depends(get_session),
@@ -42,7 +48,9 @@ def get_current_user(
     )
 
     try:
-        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
         username: str = payload.get('sub')
         if not username:
             raise credentials_exception
@@ -51,7 +59,10 @@ def get_current_user(
         raise credentials_exception
 
     user = session.scalar(
-        select(User).where(User.email == token_data.username)
+        select(User).where(
+            User.email == token_data.username
+            or User.username == token_data.username
+        )
     )
 
     if user is None:
