bfebcd2361c451803e31ee41316a25f08aba4e33
Carlos Eduardo <ducameneses16@gmail.com>
2024-02-02 02:04:18 -0300

    updating the formatting according to blue and isort

--- pizza_delivery_api/models/connection/database.py
+++ pizza_delivery_api/models/connection/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, Session
+from sqlalchemy.orm import Session, declarative_base
 
 from .secrets import Settings
 
@@ -8,6 +8,7 @@ settings = Settings()
 engine = create_engine(settings.DATABASE_URL)
 
 Base = declarative_base()
+
 
 def get_session():
     with Session(engine) as session:
