4bff462bd7ae7a9527189562c844fd31d251c5c0
Carlos Eduardo <ducameneses16@gmail.com>
2024-02-02 01:44:42 -0300

    Creating a configuration class to use environment variables

--- pizza_delivery_api/models/connection/secrets.py
+++ pizza_delivery_api/models/connection/secrets.py
@@ -1,11 +1,12 @@
-import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
-from dotenv import load_dotenv
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file='app_env.env', env_file_encoding='utf-8'
+    )
 
-load_dotenv()
+    DATABASE_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
 
-user = os.environ['POSTGRES_USER']
-password = os.environ['POSTGRES_PASSWORD']
-database = os.environ['POSTGRES_DB']
-
-connection_uri = f'postgresql://{user}:{password}@localhost:5433/{database}'
